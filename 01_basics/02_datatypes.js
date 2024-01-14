114d326f87accf569443a4fe541709c275187f5b
AthawaleShashank25 <130609495+AthawaleShashank25@users.noreply.github.com>
2024-01-14 09:58:48 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all JS code an newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3)     // not do that (code redability)
+
+let name = "shashank"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// object
+
+console.log(typeof "shashank")
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof null)           // type is object
+console.log(typeof undefined)      // type is undefined
