aafd326aca1343f65fb957c0a93920900768bf00
AthawaleShashank25 <130609495+AthawaleShashank25@users.noreply.github.com>
2024-01-22 21:34:00 +0000

    for of loop

---
+++ 05_iteration/03_loops.js
@@ -0,0 +1,44 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {      // for of loop in array
+    console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {          // for of loop in string
+    console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+console.log(map);
+
+for (const [key, value] of map) {
+     console.log(key, ':-', value);
+}
+
+
+
+
+// Note : object ke liye forof loop nhi lagta
+
+// const myObject = {
+//     game1: 'NFS',
+//     game2: 'Spiderman'
+// }
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
